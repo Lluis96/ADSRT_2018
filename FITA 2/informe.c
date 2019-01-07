d698ce876b9b5f0d846705ea857ed5dbd27a138e
Your Name <alumne@lae2009.(none)>
2019-01-07 19:11:23 +0100

    Informe que lee de database

--- FITA 2/informe.c
+++ FITA 2/informe.c
@@ -1,21 +1,21 @@
-// Per compilar el programa :  //gcc informe.c  -lsqlite3 -o informe
-// Per compilar el programa per la raspberry: arm-linux-gnueabi-gcc informe.c -o informe -L. -lsqlite3 -I/usr/include (imporntat que la llibreria  libsqlite3.so) 
+// programa per provar recollir dades durant 1 dia
+//Per compilar: gcc informe.c -lsqlite3 -lrt -lpthread -o informe
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-#endif
+#endif 
 
-#include <fcntl.h>
-#include <termios.h>
-#include <sys/ioctl.h>
+#include <fcntl.h>                                                        
+#include <termios.h>       
+#include <sys/ioctl.h> 
 #include <sys/time.h>
-#include <time.h>
+#include <time.h> 
 #include <pthread.h>
 #include <signal.h>
 #include <sys/time.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <sqlite3.h>
+#include <stdlib.h> 
+#include <sqlite3.h> 
 #include <getopt.h>
 #include <string.h>
 
@@ -23,17 +23,11 @@
 #include <funcions.h> 
 
 
-
-
-/*! comunicacio important a B115200  */
-#define BAUDRATE B115200  //IMPORTANTE QUE SEA 115200
-/*! activar fila dev/ttySO quan es vulgui comunicar amb Raspberry  */
+#define BAUDRATE B115200  //IMPORTANTE QUE SEA 115200                                                
 //#define MODEMDEVICE "/dev/ttyS0"        //Conexió IGEP - Arduino
-/*! activar fila dev/ttyACM0 quan es vulgui comunicar amb Raspberry  */
-#define MODEMDEVICE "/dev/ttyACM0"         //Conexió directa PC(Linux) - Arduino
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define MODEMDEVICE "/dev/ttyACM0"         //Conexió directa PC(Linux) - Arduino                                   
+#define _POSIX_SOURCE 1 /* POSIX compliant source */        
 
-//-------------start declaració variables de sql--------------------------------------------
 
 sqlite3 *db;
 char *zErrMsg = 0;
@@ -43,32 +37,136 @@ char *query = NULL;
 
 sqlite3_stmt *stmt;
 
-//-------------Final declaració variables de sql--------------------------------------------
-
-//-------------start declaració ordres de sql--------------------------------------------
-char *sqlTmax= "SELECT MAX (TEMPERATURA) FROM TEMPERATURA WHERE DATA BETWEEN  '26/10/2018 00:00:00' AND '26/10/2018 23:59:59';";
-char *sqlTmin= "SELECT MIN (TEMPERATURA) FROM TEMPERATURA WHERE DATA BETWEEN  '26/10/2018 00:00:00' AND '26/10/2018 23:59:59';";
-char *sqlTavg= "SELECT AVG (TEMPERATURA) FROM TEMPERATURA WHERE DATA BETWEEN  '26/10/2018 00:00:00' AND '26/10/2018 23:59:59';";
-char *sqldatainici= "SELECT (DATA) FROM TEMPERATURA order by DATA asc limit 1;";	//char *sql4= "SELECT (Data_i_Hora) WHERE Id=1 FROM historic5;";
-char *sqldatafinal= "SELECT (DATA) FROM TEMPERATURA order by DATA desc limit 1;"; //////////////////
-char *sqltotaltimefan= "SELECT SUM(TEMPS_ON) FROM ALARMES ;";
-
-//triar entre taula alarmes o temperatura
-//char *sqlavgtimefan= "SELECT avg (TEMPS_ON) FROM ALARMES;"; 
-char *sqlavgtimefan= "SELECT avg (VENT) FROM TEMPERATURA;";
-
-
-char *sqlcounttimefan= "SELECT COUNT (VENT) FROM TEMPERATURA WHERE VENT > 0;";
-char *sqlalarmestable= "SELECT * FROM ALARMES;";
-char *sqltemp= "SELECT * FROM TEMPERATURA;";
-//-------------Final declaració ordres de sql-------------------------------------------
-char variable[30];
-char dataalarma[30];
-char ton_fan[30];
-
-char *De="1393272@campus.euss.org";
-char *To="1393272@campus.euss.org";
+// Defineix punter a una estructura tm
+    struct tm * p_data;
+    char fecha[80];
+    char fecha_daybefore[80];
+    char fecha_daybefore_dos[80];
+    char fecha_day[80];
+    char dia [10];
+    char dia_before [10];
+	//-------------start variables ordres de sql--------------------------------------------
+	char sqlTmax[80],sqlTmin[80],sqlTavg[80],sqlavgtimefan[80],sqlcounttimefan[80],sqlalarmestable[80];
 	
+	char *sqldatainici= "SELECT (DATA) FROM TEMPERATURA order by DATA asc limit 1;";	//char *sql4= "SELECT (Data_i_Hora) WHERE Id=1 FROM historic5;";
+	char *sqldatafinal= "SELECT (DATA) FROM TEMPERATURA order by DATA desc limit 1;"; //////////////////
+	char *sqltotaltimefan= "SELECT SUM(TEMPS_ON) FROM ALARMES ;";
+	char *sqltemp= "SELECT * FROM TEMPERATURA;";
+	
+    int op;
+    
+	char variable[80]; //es fa servir callback2
+	char dataalarma[30]; //es fa servir callback
+	char ton_fan[30]; //es fa servir callback
+
+	char *De="1393272@campus.euss.org";
+	char *To="1393272@campus.euss.org";
+    
+    // variables on guardar les consultes sql
+	char Tmax[80],Tmin[80],Tavg[80],startdata[80], finishdata[80],tempsontotal[80],tempsavgtotal[80],numcopsfan[80]; 
+
+typedef void (timer_callback) (union sigval);
+/* Funció set_timer
+ * 
+ * Crear un timer
+ * 
+ * Paràmetres:
+ * timer_id: punter a una estructura de tipus timer_t
+ * delay: retard disparament timer (segons)
+ * interval: periode disparament timer  (segons)
+ * func: funció que s'executarà al disparar el timer
+ * data: informació que es passarà a la funció func
+ * 
+ * */
+/**
+ * @brief Funció per configurar el timer                
+ */ 
+int set_timer(timer_t * timer_id, float delay, float interval, timer_callback * func, void * data) 
+{
+    int status =0;
+    struct itimerspec ts;
+    struct sigevent se;
+
+    se.sigev_notify = SIGEV_THREAD;
+    se.sigev_value.sival_ptr = data;
+    se.sigev_notify_function = func;
+    se.sigev_notify_attributes = NULL;
+
+    status = timer_create(CLOCK_REALTIME, &se, timer_id);
+
+    ts.it_value.tv_sec = abs(delay);
+    ts.it_value.tv_nsec = (delay-abs(delay)) * 1e09;
+    ts.it_interval.tv_sec = abs(interval);
+    ts.it_interval.tv_nsec = (interval-abs(interval)) * 1e09;
+
+    status = timer_settime(*timer_id, 0, &ts, 0);
+    return 0;
+}
+
+void fecha_informe (){
+
+    
+    //Definim una variable de tipus time_t
+	time_t temps;
+	//Capturem el temps amb la funcio time(time_t *t);
+	temps = time(NULL);
+	    
+    /*---------------------------------------------------------------------------------------------*/
+	//Funcion localtime() per traduir segons UTC a la hora:minuts:segons de la hora local
+	//struct tm *localtime(const time_t *timep);
+	
+    p_data = localtime( &temps );
+	
+	strftime(fecha, 80,"%d/%m/%Y",p_data);
+	
+	strcpy(fecha_daybefore, fecha);
+	
+	strftime(dia, 10,"%d",p_data);
+	
+	op = (dia[0]-48)*10 + dia[1]-48;
+	op = op--;
+	
+	if (op<10){
+		sprintf (dia_before,"0%d", op);
+		//printf ("dia_sql: %s\n",dia_before);
+	}
+	else {
+		sprintf (dia_before,"%d", op);
+		//printf ("dia_sql: %s\n",dia_before);
+	}
+	
+	fecha_daybefore[0]=dia_before[0];
+	fecha_daybefore[1]=dia_before[1];
+	
+	sprintf(fecha_day,"%s 23:59:59",fecha_daybefore);
+	sprintf(fecha_daybefore_dos,"%s 00:00:00",fecha_daybefore);
+	
+	printf ("fecha:\t\t%s\n",fecha_day);
+	printf ("fecha before:\t%s\n",fecha_daybefore_dos);
+	
+    /*--------------------------------------------------------------------------------------------*/
+	
+    
+    //sprintf(sqlTmin,"SELECT MIN (TEMPERATURA) FROM TEMPERATURA WHERE DATA BETWEEN  '%s' AND '%s';",fecha_daybefore_dos,fecha_day);
+   // printf ("sqlTmin:\t%s\n",sqlTmin);
+ 
+    
+    sprintf(sqlTavg,"SELECT AVG (TEMPERATURA) FROM TEMPERATURA WHERE DATA BETWEEN  '%s' AND '%s';",fecha_daybefore_dos,fecha_day);
+   // printf ("sqlTmin:\t%s\n",sqlTavg);
+    
+    sprintf(sqlavgtimefan,"SELECT AVG (VENT) FROM TEMPERATURA WHERE DATA BETWEEN  '%s' AND '%s';",fecha_daybefore_dos,fecha_day);
+   // printf ("sqlavgtimefan:\t%s\n",sqlavgtimefan);
+   
+    sprintf(sqlcounttimefan,"SELECT COUNT (VENT) FROM TEMPERATURA WHERE DATA BETWEEN '%s' AND '%s' AND VENT > 0;",fecha_daybefore_dos,fecha_day);
+   // printf ("sqlcounttimefan:\t%s\n",sqlcounttimefan); // S'HA DE MIRAR SI FUNCIONA CORRECTAMENT
+    
+  //  printf("sqldatainici:\t%s\n",sqldatainici);
+  //  printf("sqldatafinal:\t%s\n",sqldatafinal);
+  //  printf("sqltotaltimefan:\t%s\n",sqltotaltimefan);
+  //  printf("sqltemp:\t%s\n",sqltemp);
+  
+}
+
 //-------------Start funcio callbacksql ------------------------------------------------
 // callback que es fa servir quan el resultat de la busqueda son varis valor -> taula alarmes
 static int callbacksql(void *NotUsed, int argc, char **argv, char **azColName) {
@@ -87,10 +185,11 @@ static int callbacksql(void *NotUsed, int argc, char **argv, char **azColName) {
    printf("\n");
    return 0;
 }
+
+
 // callback que es fa servir quan el resultat de la busqueda es un valor 
 static int callbacksql2(void *NotUsed, int argc, char **argv, char **azColName) {
    int i;
-   //char variable[30];
    for(i = 0; i<argc; i++) {
       printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
       sprintf(variable, "%s\n", argv[i] ? argv[i] : "NULL");
@@ -98,14 +197,8 @@ static int callbacksql2(void *NotUsed, int argc, char **argv, char **azColName) 
    
    return 0;
 }
-//-------------Final funcio callbacksql ------------------------------------------------
-
-//-------------Start creació del informe -----------------------------------------------
-//char montarinforme (char *datahorainici, char *datahorafinal, int *tempmax, int *tempmin, int *tempavg, int *totaltime, int *timeavg, int *venttimes){
 
 
-//}
-//-------------Final creació del informe -----------------------------------------------
 
 int main(int argc, char ** argv)
 {
@@ -130,44 +223,45 @@ int main(int argc, char ** argv)
 		}
 	}
 	
-	
-	
-	
-	
-	char Tmax[30],Tmin[30],Tavg[30],startdata[30], finishdata[30],tempsontotal[30],tempsavgtotal[30],numcopsfan[30]; // variables on guardar les consultes sql
 	char basedatos[50]="database.db";
+	/* Open database */
+	rc = sqlite3_open(basedatos, &db);
 	
-	//-------------------start getopt per passar parametres per la línea de comandes------------------------------
-	//---------------------------Final getopt---------------------------------------------------------
-	//---------------------------Start open database--------------------------------------------------
-		/* Open database */
-   rc = sqlite3_open(basedatos, &db);
-
-   if( rc ) {
+	if( rc ) {
       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
       return(0);
    } else {
       fprintf(stdout, "Opened database successfully\n");
    }
-   //---------------------------Final open database---------------------------------------------------
    
-   //---------------------------Start instruccions sql------------------------------------------------
-	
-    // Llista alarmes 
-	rc = sqlite3_exec(db, sqlalarmestable, callbacksql, 0, &zErrMsg);
+	fecha_informe();
 
-	   if( rc != SQLITE_OK ){
-	   fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		  sqlite3_free(zErrMsg);
-	   } else {
-		  fprintf(stdout, "Select datos correctamente\n");
-	   }
+	//// 1- Llista alarmes
+	//printf ("1-Llista alarmes: \n"); 
+	//sprintf(sqlalarmestable,"SELECT * FROM ALARMES WHERE DATA BETWEEN  '%s' AND '%s';",fecha_daybefore,fecha_day);
+   // printf ("sqlalarmestable:\t%s\n",sqlalarmestable); // S'HA DE MIRAR SI FUNCIONA CORRECTAMENT
+	//printf ("SENTENCIA SQL: %s \n",sqlalarmestable); 
+	//rc = sqlite3_exec(db, sqlalarmestable, callbacksql, 0, &zErrMsg);
+
+	   //if( rc != SQLITE_OK ){
+	   //fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		  //sqlite3_free(zErrMsg);
+	   //} else {
+		  //fprintf(stdout, "Select datos correctamente\n");
+	   //}
 	   
-	   printf ("%s\n",dataalarma);
-	   printf ("%s\n",ton_fan);
-	   
-	 // busquem la temperatura maxima de la base de dades  
-	 rc = sqlite3_exec(db, sqlTmax, callbacksql2, 0, &zErrMsg);
+	   //printf ("data alarma: %s\n",dataalarma);
+	   //printf ("ton_fan: %s\n",ton_fan);
+	  
+	 // 2- Busquem la temperatura maxima de la base de dades  
+	printf ("fecha:\t\t%s\n", fecha_day);
+	printf ("fecha before:\t%s\n",fecha_daybefore_dos);
+	
+	printf ("2- Busquem la temperatura maxima de la base de dades: \n"); 
+    sprintf(sqlTmax,"SELECT MAX (TEMPERATURA) FROM TEMPERATURA WHERE DATA BETWEEN  '%s' AND '%s';",fecha_daybefore_dos,fecha_day);
+    printf ("sqlTmax:\t%s\n",sqlTmax);
+    
+    rc = sqlite3_exec(db, sqlTmax, callbacksql2, 0, &zErrMsg);
 	   if( rc != SQLITE_OK ){
 	   fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		  sqlite3_free(zErrMsg);
@@ -176,8 +270,13 @@ int main(int argc, char ** argv)
 	   }
 	 strcpy(Tmax,variable);
 	 printf ("Tmax: %s\n",Tmax);
-	 
-	  // busquem la temperatura min de la base de dades  
+	
+	
+	// 3- Busquem la temperatura min de la base de dades  
+	 printf (" 3- Busquem la temperatura min de la base de dades  : \n"); 
+    sprintf(sqlTmin,"SELECT MIN (TEMPERATURA) FROM TEMPERATURA WHERE DATA BETWEEN  '%s' AND '%s';",fecha_daybefore_dos,fecha_day);
+    printf ("sqlTmax:\t%s\n",sqlTmin);
+    
 	 rc = sqlite3_exec(db, sqlTmin, callbacksql2, 0, &zErrMsg);
 	   if( rc != SQLITE_OK ){
 	   fprintf(stderr, "SQL error: %s\n", zErrMsg);
@@ -187,8 +286,8 @@ int main(int argc, char ** argv)
 	   }
 	 strcpy(Tmin,variable);
 	 printf ("Tmin: %s\n",Tmin);
-	  
-	   // busquem la temperatura min de la base de dades  
+	 
+	 // 4- Busquem la temperatura avg de la base de dades  
 	 rc = sqlite3_exec(db, sqlTavg, callbacksql2, 0, &zErrMsg);
 	   if( rc != SQLITE_OK ){
 	   fprintf(stderr, "SQL error: %s\n", zErrMsg);
@@ -197,71 +296,9 @@ int main(int argc, char ** argv)
 		  fprintf(stdout, "Select datos correctamente\n");
 	   }
 	 strcpy(Tavg,variable);
-	 printf ("Tavg: %s\n",Tavg); 
-
-   // busquem la hora inci a la base de dades  
-	 rc = sqlite3_exec(db, sqldatainici, callbacksql2, 0, &zErrMsg);
-	   if( rc != SQLITE_OK ){
-	   fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		  sqlite3_free(zErrMsg);
-	   } else {
-		  fprintf(stdout, "Select datos correctamente\n");
-	   }
-	 strcpy(startdata,variable);
-	 printf ("data inici: %s\n",startdata); 
-	 
-	 
-   // busquem la hora final a la base de dades  
-	 rc = sqlite3_exec(db, sqldatafinal, callbacksql2, 0, &zErrMsg);
-	   if( rc != SQLITE_OK ){
-	   fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		  sqlite3_free(zErrMsg);
-	   } else {
-		  fprintf(stdout, "Select datos correctamente\n");
-	   }
-	 strcpy(finishdata,variable);
-	 printf ("data final: %s\n",finishdata); 
-	 
-	 // busquem temps total funcionant del ventilador a la base de dades  
-	 rc = sqlite3_exec(db, sqltotaltimefan, callbacksql2, 0, &zErrMsg);
-	   if( rc != SQLITE_OK ){
-	   fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		  sqlite3_free(zErrMsg);
-	   } else {
-		  fprintf(stdout, "Select datos correctamente\n");
-	   }
-	 strcpy(tempsontotal,variable);
-	 printf ("Temps total ventilador ON: %s\n",tempsontotal); 
-	 
-	 // busquem temps mitja funcionant del ventilador a la base de dades  
-	 rc = sqlite3_exec(db, sqlavgtimefan, callbacksql2, 0, &zErrMsg);
-	   if( rc != SQLITE_OK ){
-	   fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		  sqlite3_free(zErrMsg);
-	   } else {
-		  fprintf(stdout, "Select datos correctamente\n");
-	   }
-	 strcpy(tempsavgtotal,variable);
-	 printf ("Temps mitja ventilador ON: %s\n",tempsavgtotal); 
-	 
-	 // busquem numero de cops funcionant del ventilador a la base de dades  
-	 rc = sqlite3_exec(db, sqlcounttimefan, callbacksql2, 0, &zErrMsg);
-	   if( rc != SQLITE_OK ){
-	   fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		  sqlite3_free(zErrMsg);
-	   } else {
-		  fprintf(stdout, "Select datos correctamente\n");
-	   }
-	 strcpy(numcopsfan,variable);
-	 printf ("Numero de cops funcionant ventilador ON: %s\n",numcopsfan);
-   //---------------------------Final instruccions sql------------------------------------------------
-  
-   char informefinal[1000];
-   
-   informe (&(informefinal[0]), startdata,finishdata,Tmax, Tmin,Tavg,tempsontotal,tempsavgtotal,numcopsfan);
-  printf("%s", De);
-	enviar_mail(De, To, informefinal);
+	 printf ("Tavg: %s\n",Tavg);
 	
-   return 0;
-   
-}
+	
+	return 0;
+	
+	}
