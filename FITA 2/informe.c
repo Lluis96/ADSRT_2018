e3afafb7fda2ea8ecc78f30f5da3826d85c3e058
Lluis Farnes <EMAIL>
2019-01-08 21:00:38 +0100

    informe.c con el cuerpo de texto del mail definitivo

--- FITA 2/informe.c
+++ FITA 2/informe.c
@@ -46,24 +46,24 @@ sqlite3_stmt *stmt;
     char dia [10];
     char dia_before [10];
 	//-------------start variables ordres de sql--------------------------------------------
-	char sqlTmax[80],sqlTmin[80],sqlTavg[80],sqlavgtimefan[80],sqlcounttimefan[80],sqlalarmestable[80];
+	char sqlTmax[80],sqlTmin[80],sqlTavg[80],sqlcounttimefan[80],sqlalarmestable[80];
 	
-	char *sqldatainici= "SELECT (DATA) FROM TEMPERATURA order by DATA asc limit 1;";	//char *sql4= "SELECT (Data_i_Hora) WHERE Id=1 FROM historic5;";
-	char *sqldatafinal= "SELECT (DATA) FROM TEMPERATURA order by DATA desc limit 1;"; //////////////////
-	char *sqltotaltimefan= "SELECT SUM(TEMPS_ON) FROM ALARMES ;";
-	char *sqltemp= "SELECT * FROM TEMPERATURA;";
+	//char *sqldatainici= "SELECT (DATA) FROM TEMPERATURA order by DATA asc limit 1;";	//char *sql4= "SELECT (Data_i_Hora) WHERE Id=1 FROM historic5;";
+	//char *sqldatafinal= "SELECT (DATA) FROM TEMPERATURA order by DATA desc limit 1;"; //////////////////
+	//char *sqltemp= "SELECT * FROM TEMPERATURA;";
 	
     int op;
+    int j;
     
 	char variable[80]; //es fa servir callback2
-	char dataalarma[30]; //es fa servir callback
-	char ton_fan[30]; //es fa servir callback
+	char dataalarma[30][30]; //es fa servir callback
+	char ton_fan[30][30]; //es fa servir callback
 
 	char *De="1393272@campus.euss.org";
 	char *To="1393272@campus.euss.org";
     
     // variables on guardar les consultes sql
-	char Tmax[80],Tmin[80],Tavg[80],startdata[80], finishdata[80],tempsontotal[80],tempsavgtotal[80],numcopsfan[80]; 
+	char Tmax[80],Tmin[80],Tavg[80],startdata[80], finishdata[80],tempsontotal[80],tempsavgtotal[80],numcopsfan[80],llistaalarmes[8000]; 
 
 typedef void (timer_callback) (union sigval);
 /* Funció set_timer
@@ -154,10 +154,8 @@ void fecha_informe (){
     sprintf(sqlTavg,"SELECT AVG (TEMPERATURA) FROM TEMPERATURA WHERE DATA BETWEEN  '%s' AND '%s';",fecha_daybefore_dos,fecha_day);
    // printf ("sqlTmin:\t%s\n",sqlTavg);
     
-    sprintf(sqlavgtimefan,"SELECT AVG (VENT) FROM TEMPERATURA WHERE DATA BETWEEN  '%s' AND '%s';",fecha_daybefore_dos,fecha_day);
-   // printf ("sqlavgtimefan:\t%s\n",sqlavgtimefan);
+    
    
-    sprintf(sqlcounttimefan,"SELECT COUNT (VENT) FROM TEMPERATURA WHERE DATA BETWEEN '%s' AND '%s' AND VENT > 0;",fecha_daybefore_dos,fecha_day);
    // printf ("sqlcounttimefan:\t%s\n",sqlcounttimefan); // S'HA DE MIRAR SI FUNCIONA CORRECTAMENT
     
   //  printf("sqldatainici:\t%s\n",sqldatainici);
@@ -169,22 +167,26 @@ void fecha_informe (){
 
 //-------------Start funcio callbacksql ------------------------------------------------
 // callback que es fa servir quan el resultat de la busqueda son varis valor -> taula alarmes
+
 static int callbacksql(void *NotUsed, int argc, char **argv, char **azColName) {
    int i;
    for(i = 0; i<argc; i++) {
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-      if (i==1){
-	  sprintf(dataalarma,"%s\n",azColName[i], argv[i] ? argv[i] : "NULL");
+     // printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+      if (i==0){
+	  sprintf(dataalarma[j],"%s\n", argv[i] ? argv[i] : "NULL");
+	  printf("dataalarma %s \n",dataalarma[j]);
 	  }
-      else if (i==2){
-		sprintf(ton_fan,"%s\n",azColName[i], argv[i] ? argv[i] : "NULL");  
+      else if (i==1){
+		sprintf(ton_fan[j],"%s\n", argv[i] ? argv[i] : "NULL");
+		printf("ton fan %s \n",ton_fan[j]);
 	  }
-      
-	
    }
+    j++;
    printf("\n");
    return 0;
 }
+
+
 
 
 // callback que es fa servir quan el resultat de la busqueda es un valor 
@@ -235,7 +237,7 @@ int main(int argc, char ** argv)
    }
    
 	fecha_informe();
-
+	
 	//// 1- Llista alarmes
 	//printf ("1-Llista alarmes: \n"); 
 	//sprintf(sqlalarmestable,"SELECT * FROM ALARMES WHERE DATA BETWEEN  '%s' AND '%s';",fecha_daybefore,fecha_day);
@@ -254,12 +256,10 @@ int main(int argc, char ** argv)
 	   //printf ("ton_fan: %s\n",ton_fan);
 	  
 	 // 2- Busquem la temperatura maxima de la base de dades  
-	printf ("fecha:\t\t%s\n", fecha_day);
-	printf ("fecha before:\t%s\n",fecha_daybefore_dos);
-	
-	printf ("2- Busquem la temperatura maxima de la base de dades: \n"); 
+	 
+	//printf ("2- Busquem la temperatura maxima de la base de dades: \n"); 
     sprintf(sqlTmax,"SELECT MAX (TEMPERATURA) FROM TEMPERATURA WHERE DATA BETWEEN  '%s' AND '%s';",fecha_daybefore_dos,fecha_day);
-    printf ("sqlTmax:\t%s\n",sqlTmax);
+    //printf ("sqlTmax:\t%s\n",sqlTmax);
     
     rc = sqlite3_exec(db, sqlTmax, callbacksql2, 0, &zErrMsg);
 	   if( rc != SQLITE_OK ){
@@ -269,13 +269,13 @@ int main(int argc, char ** argv)
 		  fprintf(stdout, "Select datos correctamente\n");
 	   }
 	 strcpy(Tmax,variable);
-	 printf ("Tmax: %s\n",Tmax);
+	 //printf ("Temperatura maxima: %s\n",Tmax);
 	
 	
 	// 3- Busquem la temperatura min de la base de dades  
-	 printf (" 3- Busquem la temperatura min de la base de dades  : \n"); 
+	//printf (" 3- Busquem la temperatura min de la base de dades  : \n"); 
     sprintf(sqlTmin,"SELECT MIN (TEMPERATURA) FROM TEMPERATURA WHERE DATA BETWEEN  '%s' AND '%s';",fecha_daybefore_dos,fecha_day);
-    printf ("sqlTmax:\t%s\n",sqlTmin);
+    //printf ("sqlTmax:\t%s\n",sqlTmin);
     
 	 rc = sqlite3_exec(db, sqlTmin, callbacksql2, 0, &zErrMsg);
 	   if( rc != SQLITE_OK ){
@@ -285,7 +285,7 @@ int main(int argc, char ** argv)
 		  fprintf(stdout, "Select datos correctamente\n");
 	   }
 	 strcpy(Tmin,variable);
-	 printf ("Tmin: %s\n",Tmin);
+	 printf ("Temperatura minima: %s\n",Tmin);
 	 
 	 // 4- Busquem la temperatura avg de la base de dades  
 	 rc = sqlite3_exec(db, sqlTavg, callbacksql2, 0, &zErrMsg);
@@ -296,8 +296,61 @@ int main(int argc, char ** argv)
 		  fprintf(stdout, "Select datos correctamente\n");
 	   }
 	 strcpy(Tavg,variable);
-	 printf ("Tavg: %s\n",Tavg);
+	 //printf ("Temperatura mediana: %s\n",Tavg);
+	 
+	 // 5- Busquem el temps que el ventilador ha estat funcionant   
+	 sprintf(sqlcounttimefan,"SELECT COUNT(*) FROM TEMPERATURA WHERE VENT>0 AND DATA BETWEEN '%s' AND '%s' ;",fecha_daybefore_dos,fecha_day);
+	 rc = sqlite3_exec(db, sqlcounttimefan, callbacksql2, 0, &zErrMsg);
+	   if( rc != SQLITE_OK ){
+	   fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		  sqlite3_free(zErrMsg);
+	   } else {
+		  fprintf(stdout, "Select datos correctamente\n");
+	   }
+	 strcpy(numcopsfan,variable);
+	 strcpy(tempsontotal,variable);
+	 
+	 int numcopsfanint;
 	
+	 numcopsfanint = atoi (numcopsfan);
+	 printf("Minutos totales encendido: %d \n\n",numcopsfanint);
+	 float mediafan = numcopsfanint/1440.0;
+	 printf ("Temps mitja de funcionant el ventilador (s): %f\n",mediafan);
+
+	j=0; 
+	//6- Llista alarmes 
+	sprintf(sqlalarmestable,"SELECT * FROM ALARMES WHERE DATA BETWEEN '%s' AND '%s' ;",fecha_daybefore_dos,fecha_day);
+	rc = sqlite3_exec(db, sqlalarmestable, callbacksql, 0, &zErrMsg);
+	   if( rc != SQLITE_OK ){
+	   fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		  sqlite3_free(zErrMsg);
+	   } else {
+		  fprintf(stdout, "Select datos correctamente\n");
+	   }
+	  
+	// Generació del informe
+	printf("------------------Generem informe------------------------\n");
+	sprintf(llistaalarmes,"%s\nData/hora inici %s\n",llistaalarmes,fecha_daybefore_dos);
+	sprintf(llistaalarmes,"%s\nData/hora final %s\n",llistaalarmes,fecha_day);
+	sprintf (llistaalarmes,"%s\nTemperatura maxima: %s",llistaalarmes,Tmax);
+	sprintf (llistaalarmes,"%s\nTemperatura minima: %s",llistaalarmes,Tmin);
+	sprintf (llistaalarmes,"%s\nTemperatura mediana: %s",llistaalarmes,Tavg);
+	sprintf (llistaalarmes,"%s\nTemps total que ha estat funcionant el ventilador (s): %s",llistaalarmes,tempsontotal);
+	sprintf (llistaalarmes,"%s\nTemps mitja de funcionant el ventilador (s): %.2f\n",llistaalarmes,mediafan);
+	sprintf (llistaalarmes,"%s\nNumero de cops  que ha estat funcionant el ventilador (s): %s\n",llistaalarmes,numcopsfan);
+	sprintf (llistaalarmes,"\nLLista d'alarmes:\n");
+
+	int i;
+	
+	
+	sprintf (llistaalarmes,"%s\n %s %s\n",llistaalarmes,dataalarma[0],ton_fan[0]);
+	
+	for(i = 1; i<j; i++) {
+	sprintf (llistaalarmes,"%s %s %s\n",llistaalarmes,dataalarma[i],ton_fan[i]);
+	//printf ("%s %s\n",dataalarma[i],ton_fan[i]);	
+	}
+	printf ("-------------------------------------\n");
+	printf ("%s\n",llistaalarmes);
 	
 	return 0;
 	
